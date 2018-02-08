1717cda2b71f4b69e83941ca1797c499a10836c7
Andres Guachisaca <caguachisaca@gmail.com>
2018-02-08 12:29:59 -0500

    Revert "init commit"
    This reverts commit 9702928bd41ebec72d51f4b05cf0d040add3b4d1.

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import Playlist from './src/playlist/components/playlist';
-import data from './src/api.json';
-
-const app = document.getElementById('app');
-
-//render(<Media title="Qué es responsive web Design ?" author="Andres Guachisaca" image="./images/covers/responsive.jpg"/>,app);
-render(<PlayList />,app);
