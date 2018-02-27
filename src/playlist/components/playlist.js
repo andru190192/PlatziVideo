880c3225cd83d96f1a7f62f45b10c76866d9e53e
Andres Guachisaca <caguachisaca@gmail.com>
2018-02-26 23:47:30 -0500

    componentes del video

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -3,17 +3,16 @@ import Media from './media.js';
 import './playlist.css';
 
 function Playlist(props) {
-  const playlist = props.data.categories[0].playlist
-  console.log(props.data);
   return (
     <div className="Playlist">
       {
-        playlist.map((item) => {
-          return <Media {...item} key={item.id} />
+        props.playlist.map((item) => {
+          return <Media handleClick={props.handleOpenModal} {...item} key={item.id} />
         })
       }
     </div>
   )
 }
+
 
 export default Playlist;
