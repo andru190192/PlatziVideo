bc5ee2c016c18d70cbfba1317dc80dea9133d145
Andres Guachisaca <caguachisaca@gmail.com>
2018-03-16 10:59:57 -0500

    controles volumen, fullscreen, tiempo

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -1,53 +1,55 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import HomeLayout from '../components/home-layout';
 import Categories from '../../categories/components/categories';
 import Related from '../components/related';
 import ModalContainer from '../../widgets/containers/modal';
 import Modal from '../../widgets/components/modal';
 import HandleError from '../../error/containers/handle-error';
+import VideoPlayer from '../../player/containers/video-player';
 
 class Home extends Component {
 
-  state = {
-    modalVisible: false,
-  }
+    state = {
+        modalVisible: false,
+    }
 
-  handleOpenModal = () =>{
-    this.setState({
-      modalVisible: true,
-    })
-  }
+    handleOpenModal = (media) => {
+        this.setState({
+            modalVisible: true,
+            media
+        })
+    }
 
-  handleCloseModal = (event) => {
-    this.setState({
-      modalVisible: false,
-    })
-  }
+    handleCloseModal = (event) => {
+        this.setState({
+            modalVisible: false,
+        })
+    }
 
-  componentDidCatch(error, info) {
-    this.setState({
-      handleError: true,
-    })
-  }
+    componentDidCatch(error, info) {
+        this.setState({
+            handleError: true,
+        })
+    }
 
-  render() {
-    return (
-      <HandleError>
-        <HomeLayout>
-          <Related />
-          <Categories categories={this.props.data.categories} handleOpenModal={this.handleOpenModal} />
-          {
-            this.state.modalVisible &&
-            <ModalContainer>
-              <Modal handleClick={this.handleCloseModal}>
-                <h1>Esto es un portal</h1>
-              </Modal>
-            </ModalContainer>
-          }
-        </HomeLayout>
-      </HandleError>
-    )
-  }
+    render() {
+        return (
+            <HandleError>
+                <HomeLayout>
+                    <Related/>
+                    <Categories categories={this.props.data.categories} handleOpenModal={this.handleOpenModal}/>
+                    {
+                        this.state.modalVisible &&
+                        <ModalContainer>
+                            <Modal handleClick={this.handleCloseModal}>
+                                <VideoPlayer autoplay src={this.state.media.src} title={this.state.media.title}/>
+                            </Modal>
+                        </ModalContainer>
+                    }
+                </HomeLayout>
+            </HandleError>
+        )
+    }
 }
 
 export default Home
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,48 +1,50 @@
-import React, { PureComponent  } from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import './media.css';
 
 
-class Media extends PureComponent  {
-  // constructor(props) {
-  //   super(props)
-  //   this.handleClick = this.handleClick.bind(this);
-  // }
-  handleClick = (event) => {
-    console.log(this.props.cover)
-  }
-  render() {
-    const styles = {
-      container: {
-        color: '#44546b',
-        cursor: 'pointer',
-        width: 260,
-        border: '1px solid red'
-      }
+class Media extends PureComponent {
+    // constructor(props) {
+    //   super(props)
+    //   this.handleClick = this.handleClick.bind(this);
+    // }
+    handleClick = (event) => {
+        console.log(this.props.cover);
+        this.props.openModal(this.props);
     }
-    return (
-      <div className="Media" onClick={this.props.handleClick}>
-        <div className="Media-cover">
-          <img
-            src={this.props.cover}
-            alt=""
-            width={260}
-            height={160}
-            className="Media-image"
-          />
-          <h3 className="Media-title">{this.props.title}</h3>
-          <p className="Media-author">{this.props.author}</p>
-        </div>
-      </div>
-    )
-  }
+
+    render() {
+        const styles = {
+            container: {
+                color: '#44546b',
+                cursor: 'pointer',
+                width: 260,
+                border: '1px solid red'
+            }
+        }
+        return (
+            <div className="Media" onClick={this.handleClick}>
+                <div className="Media-cover">
+                    <img
+                        src={this.props.cover}
+                        alt=""
+                        width={260}
+                        height={160}
+                        className="Media-image"
+                    />
+                    <h3 className="Media-title">{this.props.title}</h3>
+                    <p className="Media-author">{this.props.author}</p>
+                </div>
+            </div>
+        )
+    }
 }
 
 Media.propTypes = {
-  cover: PropTypes.string,
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string,
-  type: PropTypes.oneOf(['video', 'audio']),
+    cover: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string,
+    type: PropTypes.oneOf(['video', 'audio']),
 }
 
 export default Media;
